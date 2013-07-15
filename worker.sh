3e40c486938043630b214ababa2eedc6cdb647f0
Pavel Patrin <pavel.patrin@tatar.ru>
2013-07-15 10:22:23 +0400

    Changes in worker

--- worker.sh
+++ worker.sh
@@ -84,14 +84,13 @@ do
 
 	# Run task
 	worker_log "Running task: $TASK"
-	TASK_RETURN=$?
-	worker_log "Running task finished with code $?: $TASK"
-
 	if worker_run_task "$TASK"
 	then
+		worker_log "Running task finished with code $?: $TASK"
 		worker_log "Executing SUCC command: $SUCC"
 		worker_run_task "$SUCC"
 	else 
+		worker_log "Running task finished with code $?: $TASK"
 		worker_log "Executing FAIL command: $FAIL"
 		worker_run_task "$FAIL"
 	fi
